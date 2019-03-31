2848174c539681034778c4ea1e4a7721db08888f
MaZa <maza2003@yandex.ru>
2019-03-31 15:08:20 +0700

    Update make-protobuf.sh

--- scripts/make-protobuf.sh
+++ scripts/make-protobuf.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-cd ./SourceSdk/Interfaces/Protobuf/protobuf-2.5.0
+cd ./SourceSdk/Interfaces/Protobuf/protobuf-3.7.1
 autogenfile=./autogen.sh
 if [ ! -e "$autogenfile" ]; then
 	cd ..
-	echo "Downloading protobuf 2.5.0"
-	wget https://github.com/google/protobuf/archive/v2.5.0.tar.gz
-	tar xzvf v2.5.0.tar.gz
-	rm v2.5.0.tar.gz
-	cd protobuf-2.5.0
-	echo "Downloading gtest 1.5.0"
-	wget https://github.com/google/googletest/archive/release-1.5.0.tar.gz
-	tar xzvf release-1.5.0.tar.gz
-	rm release-1.5.0.tar.gz
-	mv googletest-release-1.5.0 gtest
+	echo "Downloading protobuf 3.7.1"
+	wget https://github.com/google/protobuf/archive/v3.7.1.tar.gz
+	tar xzvf v3.7.1.tar.gz
+	rm v3.7.1.tar.gz
+	cd protobuf-3.7.1
+	echo "Downloading gtest 1.8.1"
+	wget https://github.com/google/googletest/archive/release-1.8.1.tar.gz
+	tar xzvf release-1.8.1.tar.gz
+	rm release-1.8.1.tar.gz
+	mv googletest-release-1.8.1 gtest
 fi
